375d7be69f75dc8a79076c183e216a55e02c50e3
Patrick Stone <89548743+Pstone2@users.noreply.github.com>
2024-07-02 17:26:07 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='booklover',
+      version='0.1',
+      description='track books',
+      url='https://github.com/Pstone2/booklover/tree/main/booklover',
+      author='pstone',
+      author_email='none',
+      license='MIT',
+      packages=['booklover'])
